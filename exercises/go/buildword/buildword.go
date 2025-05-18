671a43d41ce6b2127e43f798f14f51d879b46061
gasta <gaston.e.coria@gmail.com>
2025-05-18 17:57:44 -0300

    add some go exercies, practice bfs

---
+++ exercises/go/buildword/buildword.go
@@ -0,0 +1,51 @@
+package buildword
+
+import (
+	"strings"
+
+	"github.com/eapache/queue"
+)
+
+type Pair struct {
+	s int
+	d int
+}
+
+func bfs(adj [][]int, s int, t int) int {
+	vis := make([]bool, len(adj))
+	vis[s] = true
+	q := queue.New()
+	q.Add(Pair{s, 0})
+	for q.Length() > 0 {
+		p := q.Peek().(Pair)
+		s = p.s
+		d := p.d
+		if s == t {
+			return d
+		}
+		q.Remove()
+		vis[s] = true
+		for _, v := range adj[s] {
+			if !vis[v] {
+				q.Add(Pair{v, d + 1})
+			}
+		}
+	}
+	return 0
+}
+
+func BuildWord(word string, fragments []string) int {
+	adj := make([][]int, len(word)+1)
+	for _, fragment := range fragments {
+		i, j := 0, 0
+		for j != -1 {
+			j = strings.Index(word[i:], fragment)
+			if j != -1 {
+				i += j
+				adj[i] = append(adj[i], i+len(fragment))
+				i += len(fragment)
+			}
+		}
+	}
+	return bfs(adj, 0, len(word))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gcoria/funware
+
+go 1.23.1
+
+require github.com/eapache/queue v1.1.0 // indirect
